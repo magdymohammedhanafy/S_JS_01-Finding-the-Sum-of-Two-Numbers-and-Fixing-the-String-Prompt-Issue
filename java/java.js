5e28d82da622445b8d662ef4a8539669ff1ed4f0
Magdy Mohammed Hanafy <mm312220@gmail.com>
2023-05-28 11:30:52 +0200

    First

---
+++ java/java.js
@@ -0,0 +1,9 @@
+function add()
+{
+  let numOne, numTwo, sum;
+  numOne = parseInt(document.getElementById("first").value);
+  numTwo = parseInt(document.getElementById("second").value);
+  sum = numOne + numTwo;
+  document.getElementById("answer").value = sum;
+}
+
